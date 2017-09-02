b72539b30d9c1ef100831eb9fcff6e492a043438
Thomas Kemmer <tkemmer@computer.org>
2017-09-02 11:22:28 +0200

    Make flake8 happy.

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@ def get_version(filename):
         metadata = dict(findall(r"__([a-z]+)__ = '([^']+)'", f.read()))
     return metadata['version']
 
+
 project = 'Mopidy-dLeyna'
 copyright = '2015, 2016 Thomas Kemmer'
 version = get_version(b'../mopidy_dleyna/__init__.py')
