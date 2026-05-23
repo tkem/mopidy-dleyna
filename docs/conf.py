6098ee830c8f4538e53662820d32b1c18913984f
Thomas Kemmer <tkemmer@computer.org>
2026-05-23 22:29:51 +0200

    Update build environment.

--- docs/conf.py
+++ docs/conf.py
@@ -5,23 +5,27 @@ src_directory = (pathlib.Path(__file__).parent.parent / "src").resolve()
 sys.path.insert(0, str(src_directory))
 
 
-# Extract the current version from the source.
-def get_version():
+def get_version(package):
     """Get the version and release from the source code."""
-
     text = (src_directory / "mopidy_dleyna/__init__.py").read_text()
     for line in text.splitlines():
         if not line.strip().startswith("__version__"):
             continue
-        full_version = line.partition("=")[2].strip().strip("\"'")
-        partial_version = ".".join(full_version.split(".")[:2])
-        return full_version, partial_version
+        return line.partition("=")[2].strip().strip("\"'")
 
 
 project = "Mopidy-dLeyna"
 copyright = "2015-2026 Thomas Kemmer"
-release, version = get_version()
+release = get_version(project)
+version = ".".join(release.split(".")[:2])
 
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.coverage",
+    "sphinx.ext.doctest",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.todo",
+]
 exclude_patterns = ["_build"]
 master_doc = "index"
 html_theme = "classic"
